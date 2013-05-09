867595fc966da13b841e0f8c2d99a5b2d6dff314
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2013-05-09 13:06:49 +0200

    -

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 puppet apply config/manifest.pp
 
 cp -Rv application/* /srv/www/www.gdlabs.it/application/
+rm /srv/www/www.gdlabs.it/application/index.php
+
 chown www-data:www-data /srv/www/www.gdlabs.it/application/* -R
