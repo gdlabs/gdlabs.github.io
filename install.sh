5931152749ae0d35946b9d6393eb2093b3fefdd8
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2013-07-19 02:21:29 +0200

    removed install sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-puppet apply config/manifest.pp
-
-cp -Rv application/* /srv/www/www.gdlabs.it/application/
-rm /srv/www/www.gdlabs.it/application/index.php
-
-chown www-data:www-data /srv/www/www.gdlabs.it/application/* -R
